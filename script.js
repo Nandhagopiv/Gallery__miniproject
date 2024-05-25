ec4c092b27dcb95d4ad0ccd22181e1ffcf528a27
Nandhagopiv <nandhagopy@gmail.com>
2024-05-25 12:30:09 +0530

    Update(Javascript): Updated JS file for smoother image loading.

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-var allpics = document.getElementById("allpics")
-var pics__arr = allpics.querySelectorAll("div")
-var view = document.getElementById("view")
-var closebtn = document.getElementById("closebtn")
+// var allpics = document.getElementById("allpics")
+// var pics__arr = allpics.querySelectorAll("div")
+// var view = document.getElementById("view")
+// var closebtn = document.getElementById("closebtn")
+
+// closebtn.addEventListener("click", () => {
+//     view.style.display = "none"
+// })
+
+// for (let i = 0; i < pics__arr.length; i++) {
+//     pics__arr[i].addEventListener("click", function (event) {
+//         view.style.display = "block"
+//         view.querySelector("img").classList.add("inline", "h-3/4", "md:mt-24", "mt-28")
+//         view.querySelector("img").src = event.target.currentSrc
+//     })
+// }
+
+var allpics = document.getElementById("allpics");
+var view = document.getElementById("view");
+var closebtn = document.getElementById("closebtn");
 
 closebtn.addEventListener("click", () => {
-    view.style.display = "none"
-})
+    view.style.display = "none";
+});
 
-for (let i = 0; i < pics__arr.length; i++) {
-    pics__arr[i].addEventListener("click", function (event) {
-        view.style.display = "block"
-        view.querySelector("img").classList.add("inline", "h-3/4", "md:mt-24", "mt-28")
-        view.querySelector("img").src = event.target.currentSrc
-    })
-}
+allpics.addEventListener("click", function(event) {
+    if (event.target.tagName === "IMG") {
+        view.style.display = "block";
+        view.querySelector("img").classList.add("inline", "h-3/4", "md:mt-24", "mt-28");
+        view.querySelector("img").src = event.target.currentSrc;
+    }
+});
+
